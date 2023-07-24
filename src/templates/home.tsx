f9300d0889aed1d3adbdd84446af7e01f10f8a08
Aidan Sze <asze@yext.com>
2023-07-24 14:23:52 -0400

    Home content simplified

--- src/templates/home.tsx
+++ src/templates/home.tsx
@@ -101,7 +101,7 @@ const Home = (/*{ document }: TemplateRenderProps*/) => {
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto max-w-2xl lg:max-w-4xl">
               <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                Neighborhoods
+                {/* Neighborhoods */}
               </h2>
               <div className="mt-16 space-y-20 lg:mt-20 lg:space-y-20">
                 {/* {c_featuredBlogs?.map((blog) => (
